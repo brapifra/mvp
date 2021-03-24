5efb733315fc528079d9b2fa20f49eb56259ffd9
Brais Piñeiro <brapifra@gmail.com>
2021-03-24 15:08:12 +0100

    Add custom app page with mvp.css

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const App: React.FC<any> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/mvp.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
