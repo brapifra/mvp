4a1c7a9a5a676d39d2aa98d4ffdc380d0e518a0f
Brais Piñeiro <brapifra@gmail.com>
2021-03-27 19:56:07 +0100

    Add User validator classes

---
+++ src/contexts/shared/domain/Validator.ts
@@ -0,0 +1,23 @@
+import { TranslatableText } from 'contexts/i18n'
+
+export interface Validator<T> {
+  validate(
+    field: T
+  ): { isValid: true } | { isValid: false; message: TranslatableText }
+}
+
+export class RegExpValidator implements Validator<string> {
+  constructor(
+    private readonly regExp: RegExp,
+    private readonly errorMessage: TranslatableText
+  ) {}
+
+  validate(text: string): ReturnType<Validator<string>['validate']> {
+    const isValid = this.regExp.test(text)
+
+    return {
+      isValid,
+      message: isValid ? undefined : this.errorMessage,
+    }
+  }
+}
--- src/contexts/user/domain/UserPassword.ts
+++ src/contexts/user/domain/UserPassword.ts
@@ -1,5 +1,6 @@
 // TODO: Replace "crypto" module with browser-compatible lib
 import { createHash } from 'crypto'
+import { RegExpValidator } from 'contexts/shared/domain/Validator'
 
 export class UserPassword {
   private constructor(private passwordHash: string) {}
@@ -18,3 +19,13 @@ export class UserPassword {
     return this.passwordHash
   }
 }
+
+export const UserPasswordValidator = new RegExpValidator(
+  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
+  'Use at least eight characters, one letter and one number'
+)
+
+export const UserEmailValidator = new RegExpValidator(
+  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+  'Use a valid email address'
+)
