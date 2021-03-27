e372c65131da597a2707cd3274836a5a275fe436
Brais Piñeiro <brapifra@gmail.com>
2021-03-27 20:05:11 +0100

    Amend i18n props interface

--- src/contexts/i18n/index.ts
+++ src/contexts/i18n/index.ts
@@ -1,24 +1,24 @@
-import React from 'react'
-import { GetStaticProps } from 'next'
-import esESTranslation from './es-ES.json'
+import React from 'react';
+import { GetStaticProps } from 'next';
+import esESTranslation from './es-ES.json';
 
-export type TranslatableText = keyof typeof esESTranslation
+export type TranslatableText = keyof typeof esESTranslation;
 
 const translations = {
   'es-ES': esESTranslation,
-}
+};
 
 export interface I18nStaticProps {
-  t(text: TranslatableText): string
+  locale: string;
 }
 
 export const withI18StaticProps: (fn?: GetStaticProps) => GetStaticProps = (
   fn = async () => ({ props: {} })
 ) => async (context) => {
-  const { locale, defaultLocale } = context
-  const staticProps = await fn(context)
-  const componentProps = (staticProps as any).props || {}
-  const currentLocale = locale ?? defaultLocale
+  const { locale, defaultLocale } = context;
+  const staticProps = await fn(context);
+  const componentProps = (staticProps as any).props || {};
+  const currentLocale = locale ?? defaultLocale;
 
   return {
     ...staticProps,
@@ -26,8 +26,8 @@ export const withI18StaticProps: (fn?: GetStaticProps) => GetStaticProps = (
       ...componentProps,
       locale: currentLocale,
     },
-  }
-}
+  };
+};
 
 export function useTranslate(
   locale: string
@@ -35,5 +35,5 @@ export function useTranslate(
   return React.useCallback(
     (text: string) => translations[locale]?.[text] ?? text,
     [locale]
-  )
+  );
 }
