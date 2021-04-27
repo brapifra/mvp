aa012d24c689492eb5495e3c08096f1d0d50b2ff
Brais Piñeiro <brapifra@gmail.com>
2021-04-27 18:26:54 +0200

    Add environment to GlobalConfig object

--- src/contexts/shared/domain/GlobalConfig.ts
+++ src/contexts/shared/domain/GlobalConfig.ts
@@ -3,8 +3,9 @@ export default {
   dbUser: process.env.DB_USER,
   dbName: process.env.DB_NAME,
   dbPassword: process.env.DB_PASSWORD,
-  dbPort: process.env.DB_PORT,
+  dbPort: parseInt(process.env.DB_PORT),
   tz: process.env.TZ,
   mailApiKey: process.env.MAIL_API_KEY,
   mailDomain: process.env.MAIL_DOMAIN,
+  environment: process.env.NODE_ENV,
 };
