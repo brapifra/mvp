954e2d64147f88e286fcb9e700393f91ca1fca23
Brais Piñeiro <brapifra@gmail.com>
2021-04-30 12:03:50 +0200

    Add docker-compose.yml

--- jest.global.setup.ts
+++ jest.global.setup.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
+import { execSync } from 'child_process';
 import { join } from 'path';
 
 config({ path: join(__dirname, '.env.test.local') });
@@ -6,6 +7,8 @@ config({ path: join(__dirname, '.env.test.local') });
 import { migratePostgresDatabase } from './src/contexts/shared/infrastructure/migratePostgresDatabase';
 
 export default async (): Promise<void> => {
+  execSync('docker-compose up -d');
+
   await migratePostgresDatabase(
     join(__dirname, 'src/contexts/shared/infrastructure/postgresMigrations')
   );
