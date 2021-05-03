51216bc60c47e89a6e50ad7d9d0fdc85d8465c71
Brais Piñeiro <brapifra@gmail.com>
2021-05-03 10:46:40 +0200

    UserName: Don't allow empty names

--- src/contexts/user/domain/UserName.ts
+++ src/contexts/user/domain/UserName.ts
@@ -1,7 +1,12 @@
 export class UserName {
   constructor(private name: string) {
-    if (name.length > 30) {
-      throw new LongUserNameError(name);
+    const trimmedName = name.trim();
+
+    switch (true) {
+      case trimmedName.length === 0:
+        throw new EmptyUserNameError();
+      case trimmedName.length > 30:
+        throw new LongUserNameError(name);
     }
   }
 
@@ -13,5 +18,11 @@ export class UserName {
 export class LongUserNameError extends Error {
   constructor(name: string) {
     super(`User name '${name}' is too long`);
+  }
+}
+
+export class EmptyUserNameError extends Error {
+  constructor() {
+    super("User name can't be empty");
   }
 }
--- src/contexts/user/domain/__tests__/UserName.test.ts
+++ src/contexts/user/domain/__tests__/UserName.test.ts
@@ -1,4 +1,4 @@
-import { LongUserNameError, UserName } from '../UserName';
+import { EmptyUserNameError, LongUserNameError, UserName } from '../UserName';
 
 describe('UserName', () => {
   describe('toDto', () => {
@@ -11,6 +11,12 @@ describe('UserName', () => {
     const invalidName = new Array(31).fill('a').join('');
     expect(() => new UserName(invalidName)).toThrowError(
       new LongUserNameError(invalidName)
+    );
+  });
+  it('throws an error when the name is empty', () => {
+    expect(() => new UserName('')).toThrowError(new EmptyUserNameError());
+    expect(() => new UserName(new Array(30).fill(' ').join(''))).toThrowError(
+      new EmptyUserNameError()
     );
   });
 });
