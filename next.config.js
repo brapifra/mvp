3874417ddff77db9705e1e8d8d64241f4ad8aae8
Brais Piñeiro <brapifra@gmail.com>
2021-05-04 17:16:50 +0200

    Add support for wasm files in nextjs frontend

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   i18n: { locales: ['en-GB', 'es-ES'], defaultLocale: 'es-ES' },
+  webpack: (config) => {
+    config.module.noParse = /\.wasm$/;
+    config.module.rules.push({
+      test: /\.wasm$/,
+      // Tells WebPack that this module should be included as
+      // base64-encoded binary file and not as code
+      loader: 'base64-loader',
+      // Disables WebPack's opinion where WebAssembly should be,
+      // makes it think that it's not WebAssembly
+      //
+      // Error: WebAssembly module is included in initial chunk.
+      type: 'javascript/auto',
+    });
+
+    config.node = {
+      ...config.node,
+      fs: 'empty',
+    };
+
+    return config;
+  },
 };
