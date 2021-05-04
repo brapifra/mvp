eab49fac20a7acdecb93c9a132694c4ef430fb58
Brais Piñeiro <brapifra@gmail.com>
2021-05-04 13:59:23 +0200

    Add tailwind and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import Head from 'next/head';
+import 'tailwindcss/tailwind.css';
 
 const App: React.FC<any> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <link rel="stylesheet" href="https://unpkg.com/mvp.css" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', vh + 'px');
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', vh + 'px');
+});
+`,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
+      <style global jsx>{`
+        html,
+        body {
+          height: 100vh; /* Use vh as a fallback for browsers that do not support Custom Properties */
+          height: calc(var(--vh, 1vh) * 100);
+          width: 100vw;
+        }
+
+        #__next {
+          height: 100%;
+          width: 100%;
+        }
+      `}</style>
     </>
   );
 };
